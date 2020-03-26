ce525610b109d1a703492f9edf39760224794a95
Mohd Hussain <samsteve124@gmail.com>
2020-03-26 18:26:36 +0530

    Updates

---
+++ precache-manifest.cffe994d96e257cb0c03fc401f4d9b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac06c82d3163b82def306828228f903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f9bc11f95d560923446",
+    "url": "/static/css/main.a4496a7e.chunk.css"
+  },
+  {
+    "revision": "37b667ff4c89c3a71300",
+    "url": "/static/js/2.b47d620c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b47d620c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9bc11f95d560923446",
+    "url": "/static/js/main.5b1d7d06.chunk.js"
+  },
+  {
+    "revision": "115fc5a867dda48d4dc0",
+    "url": "/static/js/runtime-main.53672eda.js"
+  }
+]);
